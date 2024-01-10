422e93948e8e17b2664dbc50d7fda6d05759e4be
taskoft <thief811@gmail.com>
2024-01-10 13:02:30 +0300

    Loginscreen düzeltme

--- lib/pages/login_screen_page.dart
+++ lib/pages/login_screen_page.dart
@@ -11,37 +11,39 @@ class _LoginScreenPageState extends State<LoginScreenPage> {
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.bottomCenter,
-          child: const Column(children: [
-            Icon(
-              Icons.no_meals,
-              size: 250,
-            ),
-            Row(
-              children: [Text("Login"), Text("Sing-up")],
-            )
-          ]),
-        ),
-        IndexedStack(
-          index: currentIndex,
-          children: [
-            Container(
-              child: Column(children: [
-                const Text("Email address"),
-                const TextField(),
-                const Text("Email address"),
-                const TextField(),
-                TextButton(
-                    onPressed: () {}, child: const Text("Forgot passcode?")),
-                TextButton(onPressed: () {}, child: const Text("Login")),
-              ]),
-            ),
-          ],
-        )
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: const Column(children: [
+              Icon(
+                Icons.no_meals,
+                size: 250,
+              ),
+              Row(
+                children: [Text("Login"), Text("Sing-up")],
+              )
+            ]),
+          ),
+          IndexedStack(
+            index: currentIndex,
+            children: [
+              Container(
+                child: Column(children: [
+                  const Text("Email address"),
+                  const TextField(),
+                  const Text("Email address"),
+                  const TextField(),
+                  TextButton(
+                      onPressed: () {}, child: const Text("Forgot passcode?")),
+                  TextButton(onPressed: () {}, child: const Text("Login")),
+                ]),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
